8dc9ddbe804d6e069d4b4915ea5f7e58b8505a4f
Aknathpanchal <100464802+Aknathpanchal@users.noreply.github.com>
2023-07-08 21:28:13 +0530

    add daisyui

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+
+function page() {
+  return (
+    <div>
+      <p>hello my name is shivaji</p>
+    </div>
+  )
+}
+
+export default page
---
+++ app/test/shree/page.tsx
@@ -0,0 +1,11 @@
+
+
+function page() {
+  return (
+    <div>
+      <p>mu name is rushikesh</p>
+    </div>
+  )
+}
+
+export default page
