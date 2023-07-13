2c0f7ee07c4c606c72a0ced4f56ae4c10906988c
Aknathpanchal <100464802+Aknathpanchal@users.noreply.github.com>
2023-07-13 11:38:27 +0530

    toast

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        // API_URL:"https://ill-rose-cormorant-boot.cyclic.app",
-        API_URL:"http://localhost:3002"
+        API_URL:"https://ill-rose-cormorant-boot.cyclic.app",
+        // API_URL:"http://localhost:3002"
     }
 }
 
